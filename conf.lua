7291a45c366338d2c4d03e431081b4fa4119c3d7
Glenn Essex <gessex@protonmail.com>
2026-01-30 22:21:48 -0500

    removing AI stuff

--- conf.lua
+++ conf.lua
@@ -4,10 +4,9 @@ function love.conf(t)
 	t.console = false
 
 	t.window.title = "exoplanet-1993"
-	t.window.width = 960
-	t.window.height = 540
-	t.window.resizable = true
-	t.window.minwidth = 480
-	t.window.minheight = 270
+	t.window.width = 800
+	t.window.height = 600
+	t.window.resizable = false
+  t.window.fullscreen = true
 	t.window.vsync = 1
 end
