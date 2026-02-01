47fc0dbcc4b007cfa6d8540f015164480b34d754
ImranB <imranbharadia@gmail.com>
2026-02-01 17:30:21 -0500

    added window lines to contain emails and subject\sender. -ImranB

--- email.lua
+++ email.lua
@@ -1,4 +1,8 @@
-email = {}
+email = {
+  emails = require "email_text"
+}
+
+
 
 function email:init()
  
@@ -7,7 +11,7 @@ function email:init()
 end
 
 function email:enter()
-  
+  print(self.emails)
 end
 
 function email:update(dt)
@@ -18,7 +22,7 @@ end
 
 function email:draw()
   simpleScale.set()
-  computer:draw()
+  --computer:draw()
   lg.setColor(1,1,1)
   lg.print("email",100,100)
   --email window bar 
@@ -30,6 +34,24 @@ function email:draw()
   --email window body
   lg.setColor(0.7, 0.7, 0.7)
   lg.rectangle("fill", 180, 90, 530, 430)
+  --division lines
+  --vertical
+  lg.setColor(0, 0, 0)
+  lg.line(330, 90, 330, 520)
+  --horizontal (line every 50px down)
+  lg.line(180, 140, 330, 140)
+  lg.line(180, 185, 330, 185)
+  lg.line(180, 225, 330, 225)
+  lg.line(180, 265, 330, 265)
+  lg.line(180, 305, 330, 305)
+  lg.line(180, 345, 330, 345)
+  lg.line(180, 385, 330, 385)
+  lg.line(180, 425, 330, 425)
+  lg.line(180, 465, 330, 465)
+  --email text
+  lg.setColor(0, 0, 0)
+  lg.printf(self.emails.email0.sender, 200, 100, 200, "left")
+  lg.printf(self.emails.email0.subject, 200, 115, 200, "left")
   --window bar and close button text
   lg.setColor(1, 1, 1)
   lg.print("eMail", 185, 65, 0, 1.25, 1.25)
