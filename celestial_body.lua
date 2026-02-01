f17b12449ac652bf1523fd344d627e86242f3dee
Glenn Essex <gessex@protonmail.com>
2026-01-31 22:53:45 -0500

    wip transit stuff

--- celestial_body.lua
+++ celestial_body.lua
@@ -1,8 +1,9 @@
 Class = require 'libs.class'
 require "libs.math_utils"
 Vector = require 'libs.vector'
-celestial_body = Class {}
+celestial_body = Class{}
 
+<<<<<<< Updated upstream
 function celestial_body:init(pos, body_radius, color)
      self.pos = Vector(pos.x, pos.y)
      self.start = Vector(pos.x, pos.y)
@@ -23,6 +24,57 @@ function celestial_body:transit(target, transit_speed, dt)
 
      self.pos.x = self.pos.x + (dir.x * transit_speed) * dt
      self.pos.y = self.pos.y + (dir.y * transit_speed) * dt
+=======
+function celestial_body:init(line,body_radius,speed,color)
+  self.line = line
+  self.pos = {x=line[1],y=line[2]}
+  self.radius = body_radius
+  self.speed = speed
+  self.color = color
+end
+
+function celestial_body:transit(dt)
+ -- print(self.line[1],self.line[2],self.line[3],self.line[4])
+  local dx,dy = self.line[3] - self.pos.x,self.line[4] - self.pos.y
+  local angle = math.atan2(dy,dx)
+  self.pos.x = self.pos.x + self.speed * math.cos(angle) * dt
+  self.pos.y = self.pos.y + self.speed * math.sin(angle) * dt
+  --[[local targetX = windowWidth - self.pos.x
+  local targetY = windowHeight - self.pos.y
+  local dx = targetX - self.pos.x
+  local dy = targetY - self.pos.y
+  local angle = math.atan2(dy,dx)
+  self.pos.x = self.pos.x + self.speed * math.cos(angle) * dt
+  self.pos.y = self.pos.y + self.speed * math.sin(angle) * dt
+  ]]--
+  --[[
+  self.velocity = self.velocity + delta * transit_speed * dt
+  -- lerp to target(x,y) based speed
+  local dir = -target 
+  local length = (dir.x^2 + dir.y ^ 2)
+  self.pos.x = self.pos.x +dir.x * transit_speed * dt
+  self.pos.y = self.pos.y + dir.y * transit_speed * dt 
+  --simple wrap
+  self.pos = wrap_vector(self.pos,Vector(windowWidth,widthHeight),self.start)
+  --print(self.pos)
+  ]]--
+  
+end
+
+
+function celestial_body: reset()
+  self.pos.x = 0
+  self.pos.y = 0
+end
+   
+function celestial_body: draw()
+  --lg.setColor(self.color)
+  --lg.circle("fill",self.x,self.y,self.radius * 1.1)
+  lg.setColor(self.color)
+  lg.circle("fill", self.pos.x,  self.pos.y, self.radius)
+end
+  
+>>>>>>> Stashed changes
 
 
      if self.pos.x < 0 or self.pos.x > windowWidth or
--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 lg = love.graphics
+random = love.math.random
 lg.setDefaultFilter("nearest", "nearest")
 Gamestate = require "libs.gamestate"
 require "libs.simpleScale"
-Vector = require 'libs.vector'
+vector = require 'libs.vector'
 width = 800
 height = 600
 windowWidth = 800
-widthHeight = 600
+windowHeight = 600
+local sign = {1,-1}
 
 
 desktopPurple = {130/255,53/255,109/255}
@@ -31,20 +33,88 @@ Starfield_bg = Starfield
 
 function init()
   simpleScale.setWindow(width,height,windowWidth,widthHeight, {fullscreen = fullscreen, resizable = true});  
+<<<<<<< Updated upstream
   star = CelestialBody(Vector(windowWidth/2,widthHeight/2),250,{1.0,0.8,0.2})
   planet = CelestialBody(Vector(0,widthHeight/2),5,{0,0,0.08})
   planet2 = CelestialBody(Vector(0,widthHeight/2),5,{0,0,0.08})
   starbg = Starfield(0.5,windowWidth,widthHeight,1)
+=======
+  star = CelestialBody({windowWidth/2,windowHeight/2,0,0},250,0,{1,1,1})
+  --planet = CelestialBody(,5,{0,0,0})
+  --planet2 = CelestialBody(Vector(0,widthHeight/2),5,{0,0,0})
+  planet = generateCelestialBody()
+  starbg = Starfield(0.5,windowWidth,windowHeight,1)
+>>>>>>> Stashed changes
   table.insert(visibleObjects,starbg)
   table.insert(visibleObjects,star)
   table.insert(visibleObjects,planet)
-  table.insert(visibleObjects,planet2)
+end
+
+function generateCelestialBody()
+  local dir = sign[random(1,2)]
+  local randomRadX = random(125)
+  local randomRadY = random(125)
+  local mx = 400+(math.sqrt(random(15625))*dir)
+  local my = 300+(math.sqrt(random(15625))*dir)
+  print (mx, my)
+  local sx = mx+(random(1000)-100)
+  local sy = my+(random(800)-100)
+  print(sx,sy)
+  local rise, run = 0,0
+  local dx,dy,ex,ey = 0,0,0,0
+  
+  if (CheckCollisionPoint(sx,sy,0,0,windowWidth,windowHeight)) then
+     if mx > sx then
+       ex = mx+(mx-sx)
+     else
+       ex = mx+(sx-mx)
+     end
+     if my > sy then
+       ey = my+(my-sy)
+     else
+       ey = my+(sy-my)
+     end
+  else
+    sy = sy+600
+    if mx > sx then
+       ex = mx+(mx-sx)
+     else
+       ex = mx+(sx-mx)
+     end
+     if my > sy then
+       ey = my+(my-sy)
+     else
+       ey = my+(sy-my)
+    end
+  end
+  print(sx,sy)
+  print(dx,dy)
+  print(ex, ey)
+  
+  --local randomAngle = love.math.random()*(2*math.pi)
+  --local pos = {x = (windowWidth/2) + (500 * math.cos(randomAngle)), y = (windowHeight/2) + (500 * math.sin(randomAngle))}
+  local pos = {sx,sy,ex,ey}
+  local rad = 5
+  local speed = love.math.random(100)
+  local color = {0,0,0}
+  return CelestialBody(pos,rad,speed,color)
+end
+  
+function CheckCollisionPoint(x1,y1,x2,y2,w,h)
+  return x1 < x2+w and
+         x2 < x1 and
+         y1 < y2+h and
+         y2 < y1
 end
 
 function love.load()
   init()
   Gamestate.registerEvents()
+<<<<<<< Updated upstream
   Gamestate.switch(titlescreen)
+=======
+  Gamestate.switch(telescope)
+>>>>>>> Stashed changes
 end
 
 function love.update(dt)
@@ -63,8 +133,13 @@ function love.resize()
 end
 
 function CelestialBodyMovements(dt)
+<<<<<<< Updated upstream
   planet:transit(Vector(windowWidth, 245) ,400, dt * simulationSpeedMultiplier)
   planet2:transit(Vector(windowWidth,-15),460,dt * simulationSpeedMultiplier)
+=======
+  planet:transit(dt)
+  --planet2:transit(Vector(windowWidth,-15),-0.4,dt * simulationSpeedMultiplier)
+>>>>>>> Stashed changes
 end
 
 
