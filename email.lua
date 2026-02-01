10166c376dd7b54480f0f58894c73d6c8267f62a
ImranB <imranbharadia@gmail.com>
2026-02-01 16:11:12 -0500

    updated window body color to not make my eyes bleed. -ImranB

--- email.lua
+++ email.lua
@@ -23,9 +23,10 @@ function email:draw()
   lg.setColor(0.8, 0.05, 0.2)
   lg.rectangle("fill",682, 62, 25, 25)
   --email window body
-  lg.setColor(1,1,1)
+  lg.setColor(0.7, 0.7, 0.7)
   lg.rectangle("fill", 180, 90, 530, 430)
   --window bar and close button text
+  lg.setColor(1, 1, 1)
   lg.print("eMail", 185, 65, 0, 1.25, 1.25)
   lg.print("X", 688.5, 63.5, 0, 1.5, 1.5)
 
