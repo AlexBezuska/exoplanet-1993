119c928b14790bef6d32602ca6a81b7f89d5fc17
ImranB <imranbharadia@gmail.com>
2026-02-01 15:13:32 -0500

    Created email window graphics base including body, upper bar, and close button. -ImranB

--- email.lua
+++ email.lua
@@ -15,6 +15,16 @@ function email:draw()
   simpleScale.set()
   lg.setColor(1,1,1)
   lg.print("email",100,100)
+  --email window bar 
+  lg.setColor(0.05, 0.2, 0.8)
+  lg.rectangle("fill", 180, 60, 530, 30)
+  --email window close button
+  lg.setColor(0.8, 0.05, 0.2)
+  lg.rectangle("fill",682, 62, 25, 25)
+  --email window body
+  lg.setColor(1,1,1)
+  lg.rectangle("fill", 180, 90, 530, 430)
+
   simpleScale.unSet()
 end
 
